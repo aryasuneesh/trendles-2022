2131a96f773f8d3726f8852b9810fcb985fb8d0a
Akanksha Singh <akanksha.sngh660@gmail.com>
2022-03-15 13:51:18 +0530

    Added boiler plate

---
+++ src/myComponents/Content3.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Content3 = () => {
+    let contentStyle = {
+        minHeight: "100vh",
+    }
+    return (
+        <>
+            <div className="container" style={contentStyle}>
+                <div className="row">
+                    <img src="./myImages/trendles.png" />    
+
+                </div>
+            </div>
+
+        </>
+    )
+}
